59aa7d7e5b23d3a5329d84d1a02afdc965a664db
dhruv <dhruvagarwal2017@gmail.com>
2022-08-21 13:20:07 +0530

    Loading and account

--- frontend/src/components/CreatorAccount.jsx
+++ frontend/src/components/CreatorAccount.jsx
@@ -24,6 +24,8 @@ import {
 import CreatePost from "./CreatePost";
 import ethers, { utils } from "ethers";
 
+import Loading from "./Loader";
+
 export default function Creator() {
   const [isCreator, setIsCreator] = useState(false);
   const [creator, setCreator] = useState({});
@@ -31,6 +33,9 @@ export default function Creator() {
   const { address } = useAccount();
   const { data: signer } = useSigner();
   const provider = useProvider();
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState("");
+  const [subscribers, setSubscribers] = useState(0);
 
   const Creator_contract = useContract({
     addressOrName: Creator_Contract_address,
@@ -53,31 +58,41 @@ export default function Creator() {
   /// Check for creator if the user is one or Not , and accordingly render the Data
   const checkCreator = async () => {
     try {
+      setIsLoading(true);
       console.log("Checking if Creator or Not");
       const check = await Creator_contract.checkStatus(address);
       console.log(check);
       // fetch the value from the fetch
       setIsCreator(check);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
+      setIsLoading(false);
     }
   };
 
   /// fetch the creator data and ipfs CID from the Creator Contract
   const fetchCreator = async () => {
     try {
+      setIsLoading(true);
+      setMessage("loading data");
       console.log("Fetching Creator Id");
       const response = await Creator_contract.getId(address);
       const id = parseInt(response._hex);
       // separate the id value from value
       console.log(id);
       setId(id);
+      const _data = await Subscription_contract.getSubscribers(id);
+      const _subscribers = parseInt(_data._hex);
+      console.log(_subscribers);
+      setSubscribers(_subscribers);
       console.log("Fetching Creators details");
       const data = await Creator_contract.fetchCreators(id);
       console.log(data);
       // set the Data(ipfs URI) part to creator Details
       // set the Balance of the user to the balance
       setCreator(data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
@@ -128,51 +143,58 @@ export default function Creator() {
 
   return (
     <>
-      <div className={`${styles.explore}`}>
-        <h1 className={styles.section_heading}>Creator Account Details</h1>
-        <div className={`${styles.creator_section}`}>
-          <div className={styles.account_details}>
-            {/* <div className={styles.profile_image}>
+      {!isLoading ? (
+        <div className={`${styles.explore}`}>
+          <h1 className={styles.section_heading}>Creator Account Details</h1>
+          <div className={`${styles.creator_section}`}>
+            <div className={styles.account_details}>
+              {/* <div className={styles.profile_image}>
               <Image src={profile} />
             </div> */}
-            <div className={styles.wallet_details}>
-              <h2>Wallet Details</h2>
-              <h3>Address: </h3>
-              <p className={styles.address}>{creator.creatorAddress}</p>
-              <h3>Balance:</h3>
-              <p>
-                {creator.balance
-                  ? utils.formatEther(parseInt(creator.balance._hex))
-                  : 0}{" "}
-                MATIC
-              </p>
-              <div>
-                <button onClick={Withdraw} className={styles.explore_btn}>
-                  Withdraw
-                </button>
+              <div className={styles.wallet_details}>
+                <h2>Wallet Details</h2>
+                <h3>Address: </h3>
+                <p className={styles.address}>{creator.creatorAddress}</p>
+                <h3>Balance:</h3>
+                <p>
+                  {creator.balance
+                    ? utils.formatEther(parseInt(creator.balance._hex))
+                    : 0}{" "}
+                  MATIC
+                </p>
+                <h3>Subscribers : {subscribers}</h3>
+                <div>
+                  <button onClick={Withdraw} className={styles.explore_btn}>
+                    Withdraw
+                  </button>
+                </div>
+              </div>
+              <div className={styles.user_subscription}>
+                {/* <h2 className={styles.card_title}>Creator NFT</h2> */}
+                <div className={styles.creator_nft}>
+                  <Image src={creator_nft} />
+                </div>
               </div>
             </div>
-            <div className={styles.user_subscription}>
-              {/* <h2 className={styles.card_title}>Creator NFT</h2> */}
-              <div className={styles.creator_nft}>
-                <Image src={creator_nft} />
-              </div>
-            </div>
-          </div>
-          {/* <hr /> */}
+            {/* <hr /> */}
 
-          <h2 className={styles.card_title}>Content</h2>
-          <div className={styles.creator_content}>
-            {/* <div className={styles.content}>
+            <h2 className={styles.card_title}>Content</h2>
+            <div className={styles.creator_content}>
+              {/* <div className={styles.content}>
               <Image src={content1} />
             </div>
             <div className={styles.content}>
               <Image src={content2} />
             </div> */}
-            <CreatePost Id={id} />
+              <CreatePost Id={id} />
+            </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <Loading _loading={isLoading} _message={message} />
+        </>
+      )}
     </>
   );
 }
--- frontend/src/components/UserAccount.jsx
+++ frontend/src/components/UserAccount.jsx
@@ -7,6 +7,7 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 export default function UserAccount() {
   ///check the user Component and fetch the details accordingly
+  /// fetch the plan for the user
   return (
     <>
       <div className={`${styles.explore}`}>
@@ -14,13 +15,11 @@ export default function UserAccount() {
         <div className={`${styles.user_section}`}>
           <div className={styles.account_card}>
             <ConnectButton />
-
             {/* <h2 className={styles.card_title}>Wallet Details</h2>
             <h3>Address: </h3>
             <span>0xA25c5bE1324764573dE0a14ABFe0279B4291adfA</span>
             <h3>Balance: </h3>
             <span>10 MATIC</span> */}
-
             <div className={styles.user_subscription}>
               <h2 className={styles.card_title}>Subscribed to Gold Plan</h2>
               <Image src={subscription_nft} />
